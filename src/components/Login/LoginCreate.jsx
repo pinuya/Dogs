8924215ac7cf404819024dfdecf5dd6493b8731a
Tifany <tifsnunes@gmail.com>
2024-04-29 13:59:26 -0300

    login routes

--- src/components/Login/LoginCreate.jsx
+++ src/components/Login/LoginCreate.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function LoginCreate(){
     return(
         <div>
-            
+            Login Create
         </div>
     )
 }
--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { Link, json } from "react-router-dom";
 
 export default function LoginForm(){
+    const[username, setUsername] = React.useState('');
+    const[password, setPassword] = React.useState('');
+
+    function handleSubmit(event){
+        event.preventDefault()
+        fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username, password})
+        }).then(response => {console.log(response);
+            return response.json();
+        }).then(json => {
+            console.log(json);
+        });
+    }
+
     return(
-        <div>
-            
-        </div>
+        <section>
+            <h1>Login</h1>
+            <form action="" onSubmit={handleSubmit}>
+            <input type="text" value={username} onChange={({target}) => setUsername(target.value)}/>
+            <input type="text" value={password} onChange={({target}) => setPassword(target.value)}/>
+
+            <button>Entrar</button>
+            </form>
+
+            <Link to="/login/create">Cadastro</Link>
+        </section>
     )
 }
