13522ebdcf07cba516670ba236a9010bd6a477d3
Tifany <tifsnunes@gmail.com>
2024-04-29 13:25:46 -0300

    routes login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import LoginCreate from "./LoginCreate";
+import LoginPasswordLost from "./LoginPasswordLost";
+import LoginPasswordReset from "./LoginPasswordReset";
 
 export default function Login(){
     return(
         <div>
-            Login
+            <Routes>
+                <Route path='/' element={<LoginForm/>}/>
+                <Route path='create' element={<LoginCreate/>}/>
+                <Route path='lost' element={<LoginPasswordLost/>}/>
+                <Route path='reset' element={<LoginPasswordReset/>}/>
+            </Routes>
         </div>
     )
 }
---
+++ src/components/Login/LoginPasswordReset.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LoginPasswordReset() {
+  return <div></div>;
+}
