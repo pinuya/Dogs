1f1371d9e3aec4f15f273b47a78e4628197f0905
Tifany <tifsnunes@gmail.com>
2024-05-04 12:30:53 -0300

    preparing the user.jsx

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import UserHeader from "./UserHeader";
+import { Routes, Route } from "react-router-dom";
+import Feed from "../Feed/Feed";
 
 export default function User() {
     return (
-        <div>Usuário</div>
+        <section className="container">
+            <UserHeader />
+            <Routes>
+                <Route path="/" element={<Feed />} />
+            </Routes>
+        </section>
     )
 }
---
+++ src/components/User/UserHeader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function UserHeader() {
+    return (
+        <div>
+            Header USER
+        </div>
+    )
+}
