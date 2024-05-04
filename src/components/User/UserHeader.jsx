ae28b5f252297379f878f7ab0708f95ccd918596
Tifany <tifsnunes@gmail.com>
2024-05-04 15:17:25 -0300

    User Header css

--- src/components/User/UserHeader.jsx
+++ src/components/User/UserHeader.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import UserHeaderNav from "./UserHeaderNav";
+import styles from './UserHeader.module.css';
+import { useLocation } from "react-router-dom";
 
 export default function UserHeader() {
+    const [title, setTitle] = React.useState('');
+    const location = useLocation();
+
+    React.useEffect(() => {
+        setTitle(location.pathname);
+        const { pathname } = location
+
+        switch (pathname) {
+            case '/conta/post':
+                setTitle('Poste Sua Foto');
+                break;
+            case '/conta/stats':
+                setTitle('Estatísticas');
+                break;
+            default:
+                setTitle('Minha Conta');
+        }
+    }, [location])
+
     return (
-        <header>
-            <h1 className="title">Titulo</h1>
+        <header className={styles.header}>
+            <h1 className="title">{title}</h1>
             <UserHeaderNav />
         </header>
     )
