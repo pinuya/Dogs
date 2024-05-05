947bdc6e6f49102766873fb4851240e8764b30b7
Tifany <tifsnunes@gmail.com>
2024-05-05 10:46:34 -0300

    mobile menu

---
+++ src/Hooks/useMedia.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function useMedia(media) {
+    const [match, setMatch] = React.useState(null);
+
+    React.useEffect(() => {
+        function changeMatch() {
+            const { matches } = window.matchMedia(media)
+            setMatch(matches)
+        }
+        changeMatch();
+        window.addEventListener('resize', changeMatch);
+        return () => {
+            window.removeEventListener('resize', changeMatch);
+        }
+    }, [media])
+
+    return match;
+}
--- src/components/User/UserHeaderNav.jsx
+++ src/components/User/UserHeaderNav.jsx
@@ -1,34 +1,50 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { UserContext } from "../../UserContext";
 import Feed from '../../Assets/feed.svg';
 import Stats from '../../Assets/estatisticas.svg';
 import AddPhoto from '../../Assets/adicionar.svg';
 import Out from '../../Assets/sair.svg';
 import styles from './UserHeaderNav.module.css';
+import useMedia from "../../Hooks/useMedia";
 
 export default function UserHeaderNav() {
     const { userLogout } = React.useContext(UserContext);
-    const { mobile, setMobile } = React.useState(null);
+    //mobile menu
+    const mobile = useMedia('(max-width: 40rem)');
+    const [mobileMenu, setMobileMenu] = React.useState(false)
+
+    const { pathname } = useLocation();
+    React.useEffect(() => {
+        setMobileMenu(false);
+    }, [pathname]);
 
     return (
-        <nav className={styles.nav}>
-            <NavLink to="/conta" end>
-                <img src={Feed} />
-                {mobile && 'Minhas Fotos'}
-            </NavLink>
-            <NavLink to="/conta/stats">
-                <img src={Stats} />
-                {mobile && 'Estatisticas'}
-            </NavLink>
-            <NavLink to="/conta/post">
-                <img src={AddPhoto} />
-                {mobile && 'Adicionar Foto'}
-            </NavLink>
-            <button onClick={userLogout}>
-                <img src={Out} />
-                {mobile && 'Sair'}
-            </button>
-        </nav>
+        <>
+            {mobile && <button aria-label="Menu"
+                className={`${styles.mobileButton} 
+             ${mobileMenu && styles.mobileButtonActive}`}
+                onClick={() => setMobileMenu(!mobileMenu)}></button>}
+
+            <nav className={`${mobile ? styles.navMobile : styles.nav} 
+                ${mobileMenu && styles.navMobileActive}`}>
+                <NavLink to="/conta" end>
+                    <img src={Feed} />
+                    {mobile && 'Minhas Fotos'}
+                </NavLink>
+                <NavLink to="/conta/stats">
+                    <img src={Stats} />
+                    {mobile && 'Estatisticas'}
+                </NavLink>
+                <NavLink to="/conta/post">
+                    <img src={AddPhoto} />
+                    {mobile && 'Adicionar Foto'}
+                </NavLink>
+                <button onClick={userLogout}>
+                    <img src={Out} />
+                    {mobile && 'Sair'}
+                </button>
+            </nav>
+        </>
     )
 }
