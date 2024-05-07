fc49a9eca76e51f1c3240c5d8f492e6387d8cc8e
Tifany <tifsnunes@gmail.com>
2024-05-07 20:16:15 -0300

    fix modal

--- src/components/Photo/PhotoContent.jsx
+++ src/components/Photo/PhotoContent.jsx
@@ -6,25 +6,27 @@ import PhotoComments from "./PhotoComments";
 export default function PhotoContent({ data }) {
     const { photo, comments } = data;
 
-    <div className={styles.photo}>
-        <div className={styles.img}>
-            <img src={photo.src} alt={photo.title} />
-        </div>
-        <div className={styles.details}>
-            <div>
-                <p className={styles.author}>
-                    <Link to={`/profile/${photo.author}`}>@{photo.author}</Link>
-                    <span className={styles.views}>{photo.acessos}</span>
-                </p>
-                <h1 className="title">
-                    <Link to={`/photo/${photo.id}`}>{photo.title}</Link>
-                </h1>
-                <ul className={styles.attributes}>
-                    <li>{photo.peso} kg</li>
-                    <li>{photo.idade} anos</li>
-                </ul>
+    return (
+        <div className={styles.photo}>
+            <div className={styles.img}>
+                <img src={photo.src} alt={photo.title} />
             </div>
+            <div className={styles.details}>
+                <div>
+                    <p className={styles.author}>
+                        <Link to={`/profile/${photo.author}`}>@{photo.author}</Link>
+                        <span className={styles.views}>{photo.acessos}</span>
+                    </p>
+                    <h1 className="title">
+                        <Link to={`/photo/${photo.id}`}>{photo.title}</Link>
+                    </h1>
+                    <ul className={styles.attributes}>
+                        <li>{photo.peso} kg</li>
+                        <li>{photo.idade} anos</li>
+                    </ul>
+                </div>
+            </div>
+            <PhotoComments id={photo.id} comments={comments} />
         </div>
-        <PhotoComments id={photo.id} comments={comments} />
-    </div>
+    )
 }
