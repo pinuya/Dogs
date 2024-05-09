3a96398c08cac811491c0df33c56ef3c14af3e59
Tifany <tifsnunes@gmail.com>
2024-05-09 18:12:51 -0300

    image skeleton loading

--- src/components/Photo/PhotoContent.jsx
+++ src/components/Photo/PhotoContent.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import PhotoComments from "./PhotoComments";
 import PhotoDelete from "./PhotoDelete";
 import { UserContext } from "../../UserContext";
+import Image from "../Helper/Image";
 
 export default function PhotoContent({ data }) {
     const user = React.useContext(UserContext);
@@ -12,7 +13,7 @@ export default function PhotoContent({ data }) {
     return (
         <div className={styles.photo}>
             <div className={styles.img}>
-                <img src={photo.src} alt={photo.title} />
+                <Image src={photo.src} alt={photo.title} />
             </div>
             <div className={styles.details}>
                 <div>
