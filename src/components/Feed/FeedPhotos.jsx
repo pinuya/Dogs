6d178ccdf78b26a2997b2848382ec648e7390fea
Tifany <tifsnunes@gmail.com>
2024-05-10 15:49:38 -0300

    add: infinite sroll

--- src/components/Feed/FeedPhotos.jsx
+++ src/components/Feed/FeedPhotos.jsx
@@ -6,18 +6,20 @@ import Error from '../Helper/Error';
 import Loading from "../Helper/Loading";
 import styles from './FeedPhotos.module.css';
 
-export default function FeedPhotos({ user, setModalPhoto }) {
+export default function FeedPhotos({ page, user, setModalPhoto, setInfinite }) {
     const { data, loading, error, request } = useFetch();
     console.log(data)
 
     React.useEffect(() => {
         async function fetchPhotos() {
-            const { url, options } = PHOTOS_GET({ page: 1, total: 6, user });
-            const { json } = await request(url, options);
-            console.log(json);
+            const total = 3;
+            const { url, options } = PHOTOS_GET({ page, total, user });
+            const { response, json } = await request(url, options);
+            //console.log(request)
+            if (response && response.ok && json.length < total) setInfinite(false);
         }
         fetchPhotos();
-    }, [request]);
+    }, [request, user, page, setInfinite]);
 
     if (error) return <Error error={error} />;
     if (loading) return <Loading />;
