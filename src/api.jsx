8a54d0bc264e345cd6a80ca4403173effccec437
Tifany <tifsnunes@gmail.com>
2024-06-24 09:37:17 -0300

    add: stats and victory lib

--- src/api.jsx
+++ src/api.jsx
@@ -137,3 +137,15 @@ export function PASSWORD_RESET(body) {
         }
     }
 }
+
+export function GET_STATS(body) {
+    return {
+        url: API_URL + '/api/stats',
+        options: {
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer ' + window.localStorage.getItem('token'),
+            },
+        }
+    }
+}
--- src/components/User/UserStats.jsx
+++ src/components/User/UserStats.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch'
+import Loading from '../Helper/Loading'
+import Error from '../Helper/Error'
 
 export default function UserStats() {
-    return (
-        <div>
-            <Head title="Estatisticas" />
-            Estatisticas
-        </div>
-    )
+    const { data, error, loading, request } = useFetch();
+
+    React.useEffect(() => {
+        async function getData() {
+            const { url, options } = GET_STATS();
+            request(url, options)
+        }
+        getData()
+    }, [request])
+
+    if (loading) return <Loading />
+    if (error) return <Error error={error} />
+    if (data)
+
+        return (
+            <div>
+                <Head title="Estatisticas" />
+                Estatisticas
+            </div>
+        );
+    else return null;
 }
