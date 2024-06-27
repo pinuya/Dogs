5348488c62e17ccb9304b7307b0c13e12bb38bdc
Tifany <tifsnunes@gmail.com>
2024-06-27 17:42:01 -0300

    styles: userStatesGraphs style

--- src/components/User/UserStatsGraphs.jsx
+++ src/components/User/UserStatsGraphs.jsx
@@ -21,11 +21,32 @@ const UserStatsGraphs = ({ data }) => {
 
     return (
         <section className={`${styles.graph} animeLeft`}>
-            <div className={styles.total}>
+            <div className={`${styles.total} ${styles.graphItem}`}>
                 <p>Acessos: {total}</p>
             </div>
-            <div>
-                <VictoryPie data={graph} />
+            <div className={styles.graphItem}>
+                <VictoryPie
+                    data={graph}
+                    innerRadius={50}
+                    padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
+                    style={{
+                        data: {
+                            fillOpacity: .9,
+                            stroke: '#fff',
+                            strokeWidth: 2,
+                        },
+                        labels: {
+                            fontSize: 14,
+                            fill: '#333',
+                        }
+                    }}
+                />
+            </div>
+
+            <div className={styles.graphItem}>
+                <VictoryChart>
+                    <VictoryBar alignment="start" data={graph}></VictoryBar>
+                </VictoryChart>
             </div>
         </section>
     )
